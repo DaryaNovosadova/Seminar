841cb70df070028230b4f30c512d442f70002ae7
Darya <darya.novosadova@gmail.com>
2022-11-09 18:13:01 +0700

    seminar4_002

---
+++ seminar4_002/Program.cs
@@ -0,0 +1,19 @@
+﻿//принимает число -> выводит кол-во цифр в нем
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int count = CountDig(num);
+
+int CountDig(int number)
+{
+    int count = 0;
+    while(number > 0)
+    {
+        number = number / 10;
+        count++;
+        Console.Write($"{number} ");
+    }
+    return count;
+}
+Console.WriteLine();
+Console.WriteLine($"В числе {num} - {count} цифр");
