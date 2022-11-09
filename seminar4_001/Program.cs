ce93a19666b380539b1eaecde0ca9cd1f7fa879c
Darya <darya.novosadova@gmail.com>
2022-11-09 17:48:09 +0700

    seminar4_001

---
+++ seminar4_001/Program.cs
@@ -0,0 +1,19 @@
+﻿//принимает число (А), выводит сумму от 1 до А
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
+
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum += number;
+        number--;
+        Console.Write($"{sum} ");
+    }
+    Console.WriteLine();
+    return sum;
+}
