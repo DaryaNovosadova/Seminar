52607ee704a5c6bb21311f8c1b3b2724a476f22a
Darya <darya.novosadova@gmail.com>
2022-11-09 18:34:05 +0700

    seminar4_003

---
+++ seminar4_003/Program.cs
@@ -0,0 +1,18 @@
+﻿//программа принимает число N, выдает произведение от 1 до N
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Произведение цифр = {GetCompNums(num)}");
+
+int GetCompNums(int number)
+{
+    int comp = 1;
+    while(number > 1)
+    {
+        comp *= number;
+        number--;
+        Console.Write($"{comp} ");
+    }
+    Console.WriteLine();
+    return comp;
+}
